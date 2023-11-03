498f5e4fa2598e16659eb87c9ff7bf85f522041b
Thinh <101701809+thinhle19DTHD1@users.noreply.github.com>
2023-11-03 20:41:52 +0700

    update favorite_news_page.dart

--- lib/app/features/favorite_news/favorite_news_page.dart
+++ lib/app/features/favorite_news/favorite_news_page.dart
@@ -1,13 +1,13 @@
 import 'package:auto_route/annotations.dart';
-import 'package:english_project/app/common/widget/button/cupertino_button.dart';
-import 'package:english_project/app/common/widget/button/image_button.dart';
+// import 'package:english_project/app/common/widget/button/cupertino_button.dart';
+// import 'package:english_project/app/common/widget/button/image_button.dart';
 import 'package:english_project/app/common/widget/edit_text/edit_text.dart';
 import 'package:english_project/app/features/favorite_news/presentation/view/favorite_body.dart';
-import 'package:english_project/app/features/news/presentation/view/news_body.dart';
+// import 'package:english_project/app/features/news/presentation/view/news_body.dart';
 import 'package:english_project/dimens.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_phoenix/flutter_phoenix.dart';
+// import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 @RoutePage()
@@ -22,7 +22,7 @@ class FavoriteNewsPage extends StatelessWidget {
           children: [
             Row(
               children: [
-                EditText(icon: Icon(Icons.search),hinText: 'tìm kiếm',).flexible(),
+                EditText(icon: const Icon(Icons.search),hinText: 'tìm kiếm',).flexible(),
                 IconButton(
                     onPressed: () {},
                     icon: const Icon(
@@ -31,7 +31,7 @@ class FavoriteNewsPage extends StatelessWidget {
                     )),
               ],
             ),
-            SizedBox(child: FavoriteBody()).expand()
+            const SizedBox(child: FavoriteBody()).expand()
           ],
         ),
 
